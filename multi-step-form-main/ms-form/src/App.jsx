1a2c57f92a68bf941cd2b5febcdb4313df005c77
ZacksDesign <zackdgaming@gmail.com>
2023-02-06 08:52:02 -0800

    full functioning form as of now!
    Within this form I have updated the form-handling process to update and change the follow:
    
    The left side to assure which page you are on.
    
    Input fields to assure they are completed.
    
    Add the buttons to handle the change to go to the next process/page.
    
    Within those being complete time to design based off the wireframes.

--- multi-step-form-main/ms-form/src/App.jsx
+++ multi-step-form-main/ms-form/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 
 function App() {
@@ -47,9 +48,9 @@ function App() {
 
   return (
     
-  <div className=' w-[420px] rounded-md shadow-md bg-white p-5' >
+  <div className=' w-[750px] rounded-md shadow-md bg-white p-5' >
     
-    <div>
+  <div>
       {
         formArray.map((v, i) => <><div className={`w-[35px] my-3 text-white rounded-full ${formNo - 1 === i || formNo - 1 === i + 1 || formNo === formArray.length ? 'bg-blue-500' : 'bg-slate-400'} h-[35px] flex justify-center items-center`}>
         {v}
@@ -58,17 +59,48 @@ function App() {
      
     {
       formNo === 1 && <div>
-        <div className='flex flex-col mb-2'> 
+      <div className='flex flex-col mb-2'> 
           <label htmlFor='name'> Name : </label> 
-           <input value={state.name} onChange={inputHandle} className="p-2 border border-slate-400 mt-1 "></input>
-
+           <input onChange={inputHandle} type="text" name="name" placeholder='name' id='name' className="p-2 border border-slate-400 mt-1" />
         </div>
+          <div>
+          <label htmlFor='email'>Email : </label>
+          <input onChange={inputHandle} name="email" type="email" placeholder='email' id='email' className='p-2 border border-slate-400 mt-1' />
+          </div>
+          <div>
+            <label htmlFor='phnum'>Phone Number : </label>
+            <input onChange={inputHandle}   name="phnum" type="text" placeholder="Phone number" id='phnum' className='p-2 border border-slate-400 mt-1' />
+            </div>
+      <div className=''>
+        <button onClick={next} className="rounded-md w-full text-white bg-blue-800">Next</button>
+      </div>
 
+  </div>
+    }
+    {
+      formNo === 2 && <div> 
+            <div className='flex flex-col mb-2'> 
+              <label htmlFor='plans'>Plans </label>
+              <button>Arcade</button>
+            </div>
 
       </div>
     }
 
+    {
+      formNo === 3 && <div>
+        <label htmlFor='addons'>Addons </label>
+        
+        </div>
 
+    }
+
+    {
+      formNo === 4 && <div>
+        <label htmlFor='smry'>Summary </label>
+
+        </div>
+    }
 
 
   </div>
