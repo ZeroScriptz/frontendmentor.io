2c596d6b2b5a0bf9c3ee5722d42a3bc5408e2974
ZacksDesign <zackdgaming@gmail.com>
2023-02-19 18:28:15 -0800

    added the images, made the plan via array to then display same with the plan-addons

---
+++ ms-form/src/data/add-ons.js
@@ -0,0 +1,28 @@
+const addOns = [
+{
+    name: 'Online Services',
+    description: 'Access to multiplayer games',
+    price: {
+        monthly: 1,
+        yearly: 10,
+    }
+},
+{
+    name: 'Larger Storages',
+    description: 'Extra 1TB of cloud storage',
+    price: {
+        monthly: 2,
+        yearly: 20,
+    }
+},
+{
+    name: 'Customizable Profile',
+    description: 'Custome the theme of your profile',
+    price: {
+        monthly: 2,
+        yearly: 30,
+    }
+}
+]
+
+export default addOns
