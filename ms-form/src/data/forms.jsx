1c39f6c9a815c73aecd0a03060f7fd486559d062
ZacksDesign <zackdgaming@gmail.com>
2023-02-19 18:48:11 -0800

    form creation set array of components

---
+++ ms-form/src/data/forms.jsx
@@ -0,0 +1,10 @@
+import FormPersonal from '../components/forms/FormPersonal';
+import FormPlan from '../components/forms/FormPlan';
+import FormAddons from '../components/forms/FormAddons';
+import FormSummary from '../components/forms/FormSummary';
+
+const forms = [
+    {
+        component: <FormPersonal />
+    }
+]
