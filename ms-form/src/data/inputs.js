68ea7a2946a6843308e6c4e9b47e9164785ddb81
ZacksDesign <zackdgaming@gmail.com>
2023-02-19 18:38:52 -0800

    input variables created in a set array to be displayed.

---
+++ ms-form/src/data/inputs.js
@@ -0,0 +1,21 @@
+const inputs = [
+    {
+        name: 'name',
+        type: 'text',
+        label: 'Name',
+        placeholder: 'e.g. John Doe' 
+    },
+    {
+        name: 'email',
+        type: 'email',
+        label: 'Email Address',
+        placeholder: 'e.g. johndoe@gmail.com'
+    },
+    {
+        name: 'phone',
+        type: 'tel',
+        label: 'Phone Number',
+        placeholder: 'e.g. 123-456-7890'
+    }
+]
+export default inputs
